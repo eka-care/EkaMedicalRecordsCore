b81440014747623eaaaa4eddbc714c79dc3eca6a
aryaIOS <arya.vashisht@eka.care>
2025-01-23 14:18:42 +0530

    Get absolute string

--- Sources/Helpers/FileHelper.swift
+++ Sources/Helpers/FileHelper.swift
@@ -69,7 +69,7 @@ final class FileHelper {
     print("File Url is -> \(fileURL)")
     do {
       try data.write(to: fileURL, options: .atomic)
-      return fileURL
+      return fileURL.absoluteString
     } catch {
       print("Couldn't write data to document directory \(error)")
       return nil
