36e1c9d4d196091af2b57de279238a90e56ab228
aryaIOS <arya.vashisht@eka.care>
2025-07-18 13:17:23 +0530

    Cases database created

---
+++ Sources/DatabaseLayer/Cases/CaseDatabaseAdapter.swift
@@ -0,0 +1,22 @@
+//
+//  CaseDatabaseAdapter.swift
+//  EkaMedicalRecordsCore
+//
+//  Created by Arya Vashisht on 17/07/25.
+//
+
+import Foundation
+
+/**
+ This file is an adapter for the database layer. It handles any model conversion from network to database layer and vice versa.
+ */
+
+/// Model used for case insert
+public struct CaseArguementModel {
+  public var caseId: String? /// Id of the case
+  public var caseType: String? /// Type of the case
+  public var createdAt: Date? /// Created at of the case
+  public var name: String? /// Name of the case
+  public var updatedAt: Date? /// Updated at of the case
+  public var userDate: Date? /// Date of the folder added by user
+}
