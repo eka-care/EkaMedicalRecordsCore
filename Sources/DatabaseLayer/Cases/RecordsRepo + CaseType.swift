6738d228a28cfe1b99e5bbabc75f358266051bc5
Shekhar Gupta <shekhargupta@ekacareadmins-MacBook-Pro.local>
2025-07-24 12:45:12 +0530

    adding CaseType Insert, fetch In DB

---
+++ Sources/DatabaseLayer/Cases/RecordsRepo + CaseType.swift
@@ -0,0 +1,23 @@
+//
+//  RecordsRepo + CaseType.swift
+//  EkaMedicalRecordsCore
+//
+//  Created by Shekhar Gupta on 24/07/25.
+//
+
+
+
+extension RecordsRepo {
+  
+  /// Used to add new casetype
+  public func createCaseType (
+    caseTypeModel: CaseTypeModel
+  ) -> CaseType {
+    return databaseManager.createCase(from: caseTypeModel)
+  }
+  
+  ///  Used to fetch all casetypes
+  public func fetchCaseTypes() -> [CaseType] {
+      return databaseManager.fetchAllCases()
+  }
+}
