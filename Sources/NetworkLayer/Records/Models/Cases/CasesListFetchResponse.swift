b720c767a219448e7855288865787c16ebc4ecf1
ShekharEkaCare <shekhar.gupta@eka.care>
2025-08-21 12:27:35 +0530

    create case response fix

--- Sources/NetworkLayer/Records/Models/Cases/CasesListFetchResponse.swift
+++ Sources/NetworkLayer/Records/Models/Cases/CasesListFetchResponse.swift
@@ -42,7 +42,7 @@ struct CaseElement: Codable {
 // MARK: - Item
 struct Item: Codable {
   let displayName, type, hiType: String?
-  let partnerMeta: PartnerMeta
+  let partnerMeta: PartnerMeta?
   let createdAt: Int?
 
   enum CodingKeys: String, CodingKey {
@@ -53,3 +53,35 @@ struct Item: Codable {
       case createdAt = "created_at"
   }
 }
+//
+/// MARK: - CasesList
+//struct CasesList: Codable {
+//    let cases: [Case]
+//}
+//
+//// MARK: - Case
+//struct Case: Codable {
+//    let id, status: String
+//    let updatedAt: Int
+//    let item: Item
+//
+//    enum CodingKeys: String, CodingKey {
+//        case id, status
+//        case updatedAt = "updated_at"
+//        case item
+//    }
+//}
+//
+//// MARK: - Item
+//struct Item: Codable {
+//    let displayName, hiType: String
+//    let createdAt: Int
+//    let type: String?
+//
+//    enum CodingKeys: String, CodingKey {
+//        case displayName = "display_name"
+//        case hiType = "hi_type"
+//        case createdAt = "created_at"
+//        case type
+//    }
+//}
