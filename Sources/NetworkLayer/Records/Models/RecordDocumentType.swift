93a16ec30fef484869779da84531087a16fcb941
ShekharEkaCare <shekhar.gupta@eka.care>
2025-09-22 16:36:51 +0530

    DocumentType Api Response

--- Sources/NetworkLayer/Records/Models/RecordDocumentType.swift
+++ Sources/NetworkLayer/Records/Models/RecordDocumentType.swift
@@ -71,3 +71,21 @@ public enum RecordDocumentType: String, CaseIterable {
     return RecordDocumentType.allCases.first { $0.filterName == filterName }
   }
 }
+
+
+// MARK: - MRDocumentType
+struct MRDocumentType: Codable {
+  let hex: String?
+  let bgHex: String?
+  let archive: Bool?
+  let id: String?
+  let displayName: String?
+
+  enum CodingKeys: String, CodingKey {
+    case hex = "hex"
+    case bgHex = "bg_hex"
+    case archive = "archive"
+    case id = "id"
+    case displayName = "display_name"
+  }
+}
