26d6d753cda21c807f67e80984ac45fa4107675b
shekhar gupta <shekhar.gupta@eka.care>
2025-11-27 20:04:44 +0530

    optional updated At

--- Sources/DatabaseLayer/Records/RecordDatabaseAdapter.swift
+++ Sources/DatabaseLayer/Records/RecordDatabaseAdapter.swift
@@ -173,8 +173,8 @@ extension RecordDatabaseAdapter {
       documentType: documentType, // other type
       oid: CoreInitConfigurations.shared.primaryFilterID ?? "",
       thumbnail: thumbnailUrl,
-      updatedAt: Date(), // Current date
-      uploadDate: Date(), // Current date
+      updatedAt: nil, // Current date
+      uploadDate: nil, // Current date
       documentURIs: recordsPath,
       contentType: contentType.fileExtension,
       caseModels: caseModels
