327d57ce399f06623de44c6687869bf7c94c9142
shekhar gupta <shekhar.gupta@eka.care>
2026-07-17 18:21:53 +0530

    Revert "Consider Local DB as the Source of Truth for Document Sync Status (#67)" (#69)

--- Sources/DatabaseLayer/Records/RecordDatabaseAdapter.swift
+++ Sources/DatabaseLayer/Records/RecordDatabaseAdapter.swift
@@ -253,6 +253,7 @@ extension RecordDatabaseAdapter {
       insertModel.isAbhaLinked = false
     }
     
+    insertModel.syncState = RecordSyncState.upload(success: true)
     /// Assign cases array if available
     insertModel.caseIDs = networkModel.recordDocument.item.cases
     /// Form Thumbnail asynchronously
